c55aefc8831d6fa6b53d752418a01e105d9bf02e
Ashu <ashu976sincere@gmail.com>
2025-06-23 19:56:58 +0530

    Second

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
 #app.config["secret_key"] = 'uwequwyequwe-1625361253-ajsgdvhasgd-125631'
-
+application = app
 app.secret_key = os.urandom(24)  # Generates a 24-byte random key
 bootstrap = Bootstrap(app)
 Moment = moment(app)
@@ -201,7 +201,7 @@ def logout():
    
 
 if __name__ == '__main__':
- app.run(debug=True)
+ application.run(debug=True)
 
 
 
